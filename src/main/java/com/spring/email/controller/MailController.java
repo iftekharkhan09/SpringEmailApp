d28165468916000cf33d7c1f1682d57b68a3f70b
Iftekhar Ahmed Khan <iftekharkhan09@users.noreply.github.com>
2017-10-07 16:33:48 +0530

    changed.

--- src/main/java/com/spring/email/controller/MailController.java
+++ src/main/java/com/spring/email/controller/MailController.java
@@ -3,12 +3,12 @@ package com.spring.email.controller;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import com.spring.email.SimpleEmailService;
-import com.spring.email.config.SpringConfig;
+import com.spring.email.service.impl.MailSenderImpl;
 
 public class MailController {
 	public static void main(String[] args) {
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
-		SimpleEmailService simpleEmailService = ctx.getBean(SimpleEmailService.class);
-		simpleEmailService.sendEmail();
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(MailSenderImpl.class);
+		//SimpleEmailService simpleEmailService = ctx.getBean(SimpleEmailService.class);
+		//simpleEmailService.sendEmail();
 	}
 }
--- src/main/java/com/spring/email/service/impl/MailSenderImpl.java
+++ src/main/java/com/spring/email/service/impl/MailSenderImpl.java
@@ -4,18 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.stereotype.Service;
 
 @Configuration
-@Service
-public class MailSenderImpl implements com.spring.email.service.MailSender {
-	@Override
+public class MailSenderImpl{
 	@Bean
 	public MailSender mailSender() {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 		mailSender.setHost("smtp.gmail.com");
 		mailSender.setPort(465);
-		mailSender.setPassword("Prince@981");
+		mailSender.setPassword("hbhkb@981");
 		mailSender.setUsername("iftekharkhan629@gmail.com");
 		return mailSender();
 	}
